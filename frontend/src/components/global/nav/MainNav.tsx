4d32781e88398ff709c24049d36056d339174a77
KneerazPandey <nirajpandeyniraj77@gmail.com>
2024-11-18 17:50:58 +0000

    added the auth0 for the login functionality successfully

--- frontend/src/components/global/nav/MainNav.tsx
+++ frontend/src/components/global/nav/MainNav.tsx
@@ -1,9 +1,14 @@
 import { Button } from "@/components/ui/button";
+import { useAuth0 } from "@auth0/auth0-react";
 
 
 const MainNav = () => {
+  const { loginWithRedirect } = useAuth0();
+
   return (
-    <Button variant={'ghost'} className="font-semibold text-lg hover:text-orange-500 hover:bg-white">
+    <Button onClick={async () => {
+      await loginWithRedirect();
+    }} variant={'ghost'} className="font-semibold text-lg hover:text-orange-500 hover:bg-white">
       Log In
     </Button>
   );
